aa07f923b6a8912fa5e3d4537ace6a4a38c08051
Steve Weiss <weissst@mail.gvsu.edu>
2017-07-01 23:59:57 -0400

    Removed ability to customize delimiters to reduce complexity. YAGNI and what have you.

--- lib/tempeh.rb
+++ lib/tempeh.rb
@@ -1,64 +1,57 @@
 class Tempeh
   VERSION = "0.1.0"
-  BLK_OPEN = "%"
-  BLK_CLOSE = "%"
-  STR_OPEN = "{"
-  STR_CLOSE = "}"
 
-  def initialize(**opts)
-    # Allow users to provide custom delimiters for both
-    # the block and string expressions.
-    @blk_open = opts.fetch(:blk_open, BLK_OPEN)
-    @blk_close = opts.fetch(:blk_close, BLK_CLOSE)
-    @str_open = opts.fetch(:str_open, STR_OPEN)
-    @str_close = opts.fetch(:str_close, STR_CLOSE)
+  BLK_OPEN = '%'
 
-    # Prepare delimiters for use in regexp pattern.
-    escaped_blk_open,
-    escaped_blk_close,
-    escaped_str_open,
-    escaped_str_close = [
-      @blk_open,
-      @blk_close,
-      @str_open,
-      @str_close
-    ].collect { |delimiter| Regexp.escape(delimiter) }
+  BLK_CLOSE = '%'
 
-    @pattern = /
-      (#{escaped_blk_open})\s+(.*?)\s+#{escaped_blk_close} | # Multiline Ruby blocks.
-      (#{escaped_str_open})(.*?)#{escaped_str_close}         # Ruby to be escaped as a string.
-    /mx
-  end
+  STR_OPEN = '{'
 
-  def parse(raw, template = Template)
-    terms = raw.split(@pattern)
-    parts = "proc do |args = {}, __o = ''|"
+  STR_CLOSE = '}'
 
-    while (term = terms.shift)
-      case term
-      when @blk_open then parts << terms.shift << "\n"
-      when @str_open then parts << "__o << (" << terms.shift << ").to_s \n"
-      else                parts << "__o << " << term.dump << "\n"
+  PATTERN = /
+    (?<!\\)(#{BLK_OPEN})\s+(.*?)\s+#{BLK_CLOSE} | # Multiline Ruby blocks.
+    (?<!\\)(#{STR_OPEN})(.*?)#{STR_CLOSE}         # Ruby to be evaluated a string.
+  /mx
+
+  HTML_ESCAPE = {
+    "&" => "&amp;",
+    ">" => "&gt;",
+    "<" => "&lt;",
+    '"' => "&#39;",
+    "'" => "&#34;"
+  }.freeze
+
+  UNSAFE = /[&"'><]/
+
+  class << self
+    def compile(str)
+      terms = str.split(PATTERN)
+      parts = "proc do |args = {}, __o = ''|"
+
+      while (term = terms.shift)
+        case term
+        when BLK_OPEN then parts << terms.shift << "\n"
+        when STR_OPEN then parts << "__o << Tempeh.escape(" << terms.shift << ") \n"
+        else               parts << "__o << " << term.dump << "\n"
+        end
       end
+
+      parts << "__o; end"
+
+      eval(parts)
     end
 
-    parts << "__o; end"
-
-    code = eval(parts) # Compile parts into a Proc.
-
-    template.new(code)
+    def escape(val)
+      val.to_s.gsub(UNSAFE, HTML_ESCAPE)
+    end
   end
 
-  private
+  def initialize(str)
+    @code = Tempeh.compile(str)
+  end
 
-  class Template
-    def initialize(code)
-      @code = code
-    end
-
-    # Convenience for executing the code Proc in a given context.
-    def render(context = nil, **args)
-      context.instance_exec(args, &@code)
-    end
+  def render(context = nil, **args)
+    context.instance_exec(args, &@code)
   end
 end
--- test/all.rb
+++ test/all.rb
@@ -1,60 +1,45 @@
 require_relative '../lib/tempeh'
 
-setup do
-  Tempeh.new
-end
-
-test "parsing" do |t|
+test do |t|
   # empty lines
-  template = t.parse("\n\n \n")
+  template = Tempeh.new("\n\n \n")
 
   assert_equal "\n\n \n", template.render
 
   # quotes
-  template = t.parse("'foo' 'bar' 'baz'")
+  template = Tempeh.new("'foo' 'bar' 'baz'")
 
   assert_equal "'foo' 'bar' 'baz'", template.render
 
   # block expressions
-  template = t.parse("% if true %yes% else %no% end %")
+  template = Tempeh.new("% if true %yes% else %no% end %")
 
   assert_equal "yes", template.render
 
   # string expressions
-  template = t.parse("{ 'Hello' }")
+  template = Tempeh.new("{ 'Hello' }")
 
   assert_equal "Hello", template.render
 
   # nil string expressions
-  template = t.parse("{ nil }")
+  template = Tempeh.new("{ nil }")
 
   assert_equal "", template.render
+
+  # allows escaping expressions
+  template = Tempeh.new('\{ nil }\% hello %')
+
+  assert_equal '\{ nil }\% hello %', template.render
 
   # render with context
   context = Struct.new(:foo).new('bar')
 
-  template = t.parse("{ foo }")
+  template = Tempeh.new("{ foo }")
 
   assert_equal "bar", template.render(context)
 
   # render with args
-  template = t.parse("{ args[:foo] }")
+  template = Tempeh.new("{ args[:foo] }")
 
   assert_equal "bar", template.render(foo: "bar")
-end
-
-setup do
-  Tempeh.new(blk_open: "^", blk_close: ")", str_open: "[$", str_close: "]]")
-end
-
-test "custom delimiters" do |t|
-  # block expressions
-  template = t.parse("^ if true )yes^ else )no^ end )")
-
-  assert_equal "yes", template.render
-
-  # string expressions
-  template = t.parse("[$ 'Hello' ]]")
-
-  assert_equal "Hello", template.render
 end
